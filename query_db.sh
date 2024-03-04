43bbd36b88a6fd85b6693f7951383c64308a6491
Tommi Mäklin <tommi@maklin.fi>
2024-03-04 14:22:11 +0200

    Update database with clbS-like genes.

--- query_db.sh
+++ query_db.sh
@@ -10,10 +10,10 @@ tmp_refs=refs_$RANDOM".txt"
 tmp_db=$in_filename"_bifrost"
 
 ls -d $1 > $tmp_refs
-Bifrost build -t 4 -k 31 -i -d -r $tmp_refs -c -o $tmp_db
+Bifrost build -t 1 -k 31 -i -d -r $tmp_refs -c -o $tmp_db
 
-res=$in_filename"_IHE3034_pks_island_genes.fasta.search"
-bin/BlastFrost -o $in_filename -g $tmp_db".gfa" -f $tmp_db".bfg_colors" -q db/IHE3034_pks_island_genes.fasta  > /dev/null
+res=$in_filename"_db.fasta.search"
+bin/BlastFrost -o $in_filename -g $tmp_db".gfa" -f $tmp_db".bfg_colors" -q db/db.fasta  > /dev/null
 cat $res | sed 's/|locus.*bactin//g' | sed 's/,$//g' | awk '{ print $2 "\t" $1 "\t" $3 "\t" $4}'
 rm $res
 
